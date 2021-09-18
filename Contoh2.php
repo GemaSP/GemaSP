6a568211325637a113de7def4831cf233ae8b1b9
GemaSP <gesap02@gmail.com>
2021-09-18 10:50:28 +0700

    menambahkan Contoh2.php

---
+++ Contoh2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh2 extends BaseController
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya gema
+		Hobi saya main Game dan ngocok";
+	}
+}
