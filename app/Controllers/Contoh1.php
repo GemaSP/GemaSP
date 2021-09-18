f5b5b004612f5d60af21358c7bfce210f0609d95
GemaSP <gesap02@gmail.com>
2021-09-18 11:19:55 +0700

    Contoh1.php

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "Perkenalkan";
+        echo "Saya sugiono 
+        hobi saya ngewe cucu"
+    }
+}
